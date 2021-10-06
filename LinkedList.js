6604c6ebe2b5ee1d7f77f08caa0b454bc3be1100
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-10-06 15:19:30 -0700

    Create unshift and push methods for class that clear tests

---
+++ LinkedList.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(data) {
+    this.data = data; 
+    this.next = null; 
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null; 
+    this.size = 0;
+  }
+
+  unshift(data) {
+    let newNode = new Node(data); 
+
+    if (this.head === null) {
+      this.head = newNode; 
+    }
+    else {
+      // get first node
+      let node = this.head; 
+      this.head = newNode; 
+      newNode.next = node; 
+    }
+
+    this.size++; 
+  }
+
+  push(data) {
+    let newNode = new Node(data); 
+
+    if (this.head === null) {
+      this.head = newNode; 
+    }
+    else {
+      let current = this.head; 
+      while (current.next !== null) {
+        current = current.next; 
+      }
+      current.next = newNode;
+    }
+
+    this.size++; 
+  }
+
+  // shift() {
+  //   if (this.head === null) return;
+  //   else {
+  //     let node = this.head.next; 
+  //     this.head = node; 
+  //   }
+
+  //   this.size--; 
+  // }
+
+  // display() {
+  //   let current = this.head; 
+  //   while (current !== null) {
+  //     console.log(current.data); 
+  //     current = current.next; 
+  //   }
+  // }
+}
+
+module.exports = LinkedList; 
