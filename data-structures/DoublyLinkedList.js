df430e6f89ce70a92486c982cdd3a6d82d1f9cca
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-10-07 18:26:55 -0700

    Implement unshift and push methods for doubly linked list(dll)

--- data-structures/DoublyLinkedList.js
+++ data-structures/DoublyLinkedList.js
@@ -1,6 +1,7 @@
 class Node {
   constructor(data) {
     this.data = data;
+    this.prev = null; 
     this.next = null;
   }
 }
@@ -8,18 +9,21 @@ class Node {
 class DoublyLinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
     this.size = 0;
   }
 
   unshift(data) {
     let newNode = new Node(data);
 
-    if (!this.head) this.head = newNode;
-    else {
-      // get first node
-      let node = this.head;
+    if (!this.head && !this.tail) {
       this.head = newNode;
-      newNode.next = node;
+      this.tail = newNode;
+    } else {
+      let oldHeadNode = this.head;
+      this.head.prev = newNode; 
+      this.head = newNode;
+      newNode.next = oldHeadNode;
     }
 
     this.size++;
@@ -28,41 +32,130 @@ class DoublyLinkedList {
   push(data) {
     let newNode = new Node(data);
 
-    if (!this.head) this.head = newNode;
-    else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
+    if (!this.head && !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let oldTailNode = this.tail; 
+      this.tail.next = newNode;
+      this.tail = newNode;
+      newNode.prev = oldTailNode;
     }
 
     this.size++;
   }
 
+  insertAt(data, index) {
+    if (index <= 0) this.unshift(data);
+    else if (index >= this.size) this.push(data);
+    else {
+      let newNode = new Node(data);
+
+      let current = this.head;
+      // if you're inserting at index 1 and size is 2,
+      // then you need the node at index 0.
+      // so in reality, you're actually searching for index: index - 1
+      index--;
+
+      while (index > 0) {
+        current = current.next;
+        index--;
+      }
+
+      let node = current.next;
+      current.next = newNode;
+      newNode.next = node;
+
+      // in else statement because other branches
+      // call methods that do this already
+      this.size++;
+    }
+  }
+
   shift() {
     if (!this.head) return;
-    else this.head = this.head.next;
+
+    let removedNode;
+    if (!this.head.next) {
+      removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      removedNode = this.head;
+      this.head = this.head.next;
+    }
 
     this.size--;
+    return removedNode.data;
   }
 
   pop() {
-    if (!this.head) return;
-    else if (!this.head.next) this.head = null;
-    else {
+    if (!this.tail) return;
+
+    let removedNode;
+    if (!this.head.next) {
+      removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
       let current = this.head;
       while (current.next.next) {
         current = current.next;
       }
+      removedNode = current.next;
       current.next = null;
+      this.tail = current;
     }
 
     this.size--;
+    return removedNode.data;
+  }
+
+  removeAt(index) {
+    if (index <= 0) return this.shift();
+    else if (index >= this.size - 1) return this.pop();
+    else {
+      let current = this.head;
+      // if you're inserting at index 1 and size is 2,
+      // then you need the node at index 0.
+      // so in reality, you're actually searching for index: index - 1
+      index--;
+
+      while (index > 0) {
+        current = current.next;
+        index--;
+      }
+
+      let removedNode = current.next;
+      let node = current.next.next;
+      current.next = node;
+
+      // in else statement because other branches
+      // call methods that do this already
+      this.size--;
+      return removedNode.data;
+    }
   }
 
   isEmpty() {
     return this.size === 0;
+  }
+
+  at(index) {
+    if (index >= this.size || index < 0) return null;
+
+    if (this.head) {
+      let current = this.head;
+
+      while (index > 0) {
+        current = current.next;
+        index--;
+      }
+
+      return current;
+    }
+
+    return null;
   }
 
   indexOf(data) {
@@ -73,16 +166,28 @@ class DoublyLinkedList {
       current = current.next;
       index++;
     }
-    return -1;
+    return null;
   }
 
-  display() {
-    let current = this.head;
-    while (current) {
-      console.log(current.data);
-      current = current.next;
-    }
+  clear() {
+    this.head = null;
+    this.tail = null;
   }
 }
+
+// let dll = new DoublyLinkedList();
+// dll.push(1);
+// dll.push(2);
+// dll.push(3);
+
+// console.log(dll.at(0).data);
+// console.log(dll.at(0).next.data); 
+// console.log();
+// console.log(dll.at(1).prev.data);
+// console.log(dll.at(1).data); 
+// console.log(dll.at(1).next.data); 
+// console.log(); 
+// console.log(dll.at(2).prev.data); 
+// console.log(dll.at(2).data);
 
 module.exports = DoublyLinkedList;
---
+++ tests/DoublyLinkedList.test.js
@@ -0,0 +1,177 @@
+const DoublyLinkedList = require("../data-structures/DoublyLinkedList");
+
+describe("DoublyLinkedList", () => {
+  describe("#unshift", () => {
+    test("adds a new element at the beginning of list", () => {
+      let dll = new DoublyLinkedList();
+      dll.unshift(1);
+      dll.unshift(2);
+      dll.unshift(3); // 3 -> 2 -> 1 -> NULL
+
+      expect(dll.head.data).toBe(3);
+      expect(dll.tail.data).toBe(1);
+
+      expect(dll.at(1).prev.data).toBe(3);
+      expect(dll.at(1).data).toBe(2);
+      expect(dll.at(1).next.data).toBe(1);
+
+      expect(dll.size).toBe(3);
+      dll.clear();
+
+      // let ll = new DoublyLinkedList();
+      // ll.unshift("Roger"); // Roger -> NULL
+      // expect(ll.head.data).toEqual("Roger");
+      // expect(ll.tail.data).toEqual("Roger");
+      // ll.unshift("Ade"); // Ade -> Roger -> NULL
+      // expect(ll.head.data).toEqual("Ade");
+      // expect(ll.tail.data).toEqual("Roger");
+
+      // expect(ll.at(0)).toEqual("Ade");
+      // expect(ll.at(1)).toEqual("Roger");
+      // expect(ll.size).toBe(2);
+
+      // ll.clear();
+    });
+  });
+
+  describe("#push", () => {
+    test("adds a new element at the end of list", () => {
+      let dll = new DoublyLinkedList(); 
+      dll.push(1); 
+      dll.push(2); 
+      dll.push(3); // 1 -> 2 -> 3 -> null
+
+      expect(dll.head.data).toBe(1); 
+      expect(dll.tail.data).toBe(3);
+
+      expect(dll.at(1).prev.data).toBe(1); 
+      expect(dll.at(1).data).toBe(2); 
+      expect(dll.at(1).next.data).toBe(3); 
+
+      expect(dll.size).toBe(3);
+      dll.clear();
+    });
+  });
+
+  // describe("#insert", () => {
+  //   test("adds a new element at the specifed index", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.insertAt("Roger", -1); // Roger -> NULL
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Roger");
+  //     // ll.insertAt("Ade", 3); // Roger -> Ade -> NULL
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Ade");
+  //     // ll.insertAt("Juan", 1); // Roger -> Juan -> Ade -> NULL;
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Ade");
+
+  //     // expect(ll.at(0)).toEqual("Roger");
+  //     // expect(ll.at(1)).toEqual("Juan");
+  //     // expect(ll.at(2)).toEqual("Ade");
+  //     // expect(ll.size).toBe(3);
+
+  //     // ll.clear();
+  //   });
+  // });
+
+  // describe("#shift", () => {
+  //   test("removes element at the beginning of list", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.unshift("Roger"); // Roger -> NULL
+  //     // ll.unshift("Juan"); // Juan -> Roger -> NULL
+  //     // ll.unshift("Ade"); // Ade -> Juan -> Roger -> NULL
+  //     // ll.shift(); // Juan -> Roger -> NULL
+  //     // expect(ll.head.data).toEqual("Juan");
+  //     // expect(ll.tail.data).toEqual("Roger");
+  //     // expect(ll.at(0)).toEqual("Juan");
+  //     // expect(ll.size).toBe(2);
+
+  //     // ll.shift(); // Roger -> NULL
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Roger");
+  //     // expect(ll.at(0)).toEqual("Roger");
+  //     // expect(ll.size).toBe(1);
+
+  //     // ll.shift(); // NULL
+  //     // expect(ll.at(0)).toBeNull();
+  //     // expect(ll.size).toBe(0);
+  //   });
+  // });
+
+  // describe("#pop", () => {
+  //   test("removes element at the end of list", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.push("Roger"); // Roger -> NULL
+  //     // ll.push("Ade"); // Roger -> Ade -> NULL
+  //     // ll.push("Juan"); // Roger -> Ade -> Juan -> NULL
+  //     // expect(ll.pop()).toEqual("Juan"); // Roger -> Ade -> NULL
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Ade");
+  //     // expect(ll.at(1)).toEqual("Ade");
+  //     // expect(ll.size).toBe(2);
+
+  //     // expect(ll.pop()).toEqual("Ade"); // Roger -> NULL
+  //     // expect(ll.head.data).toEqual("Roger");
+  //     // expect(ll.tail.data).toEqual("Roger");
+  //     // expect(ll.at(0)).toEqual("Roger");
+  //     // expect(ll.size).toBe(1);
+
+  //     // expect(ll.pop()).toEqual("Roger"); // NULL
+  //     // expect(ll.at(0)).toBeNull();
+  //     // expect(ll.size).toBe(0);
+  //   });
+  // });
+
+  // describe("#removeAt", () => {
+  //   test("removes an element at the specifed index", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.push("Roger"); // Roger -> NULL
+  //     // ll.push("Ade"); // Roger -> Ade -> NULL
+  //     // ll.push("Juan"); // Roger -> Ade -> Juan -> NULL;
+  //     // ll.push("Emma"); // Roger -> Ade -> Juan -> Emma -> NULL;
+
+  //     // expect(ll.at(1)).toEqual("Ade");
+  //     // expect(ll.removeAt(1)).toEqual("Ade"); // Roger -> Juan -> Emma -> NULL;
+  //     // expect(ll.at(1)).toEqual("Juan");
+
+  //     // expect(ll.at(2)).toEqual("Emma");
+  //     // expect(ll.removeAt(2)).toEqual("Emma"); // Roger -> Juan -> NULL
+  //     // expect(ll.at(1)).toEqual("Juan");
+  //     // expect(ll.at(2)).toBeNull();
+
+  //     // expect(ll.at(0)).toEqual("Roger");
+  //     // expect(ll.removeAt(0)).toEqual("Roger"); // Juan -> NULL;
+
+  //     // expect(ll.removeAt(0)).toEqual("Juan"); // NULL;
+  //   });
+  // });
+
+  // describe("#isEmpty", () => {
+  //   test("checks if the list is empty", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.push("Roger"); // Roger -> NULL
+
+  //     // expect(ll.isEmpty()).toBe(false);
+
+  //     // ll.pop();
+
+  //     // expect(ll.isEmpty()).toBe(true);
+  //   });
+  // });
+
+  // describe("#indexOf", () => {
+  //   test("checks if the list is empty", () => {
+  //     // let ll = new DoublyLinkedList();
+  //     // ll.push("Roger"); // Roger -> NULL
+  //     // ll.push("Ade"); // Roger -> Ade -> NULL
+  //     // ll.push("Juan"); // Roger -> Ade -> Juan -> NULL
+
+  //     // expect(ll.indexOf("Roger")).toBe(0);
+  //     // expect(ll.indexOf("Juan")).toBe(2);
+  //     // expect(ll.indexOf("Jorge")).toBeNull();
+
+  //     // ll.clear();
+  //   });
+  // });
+});
