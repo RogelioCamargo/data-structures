9aea4cc0e89d3f47c7a55d170cb64d3e99b38b39
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-10-15 07:36:46 -0700

    Implement breadth and depth traversal using queue and stack, respectively

--- ds/BinarySearchTree.js
+++ ds/BinarySearchTree.js
@@ -15,41 +15,38 @@ class BinarySearchTree {
     let newNode = new Node(data);
 
     if (!this.root) this.root = newNode;
-    else {
-      function insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-          if (!node.left) node.left = newNode;
-          else insertNode(node.left, newNode);
-        } else {
-          if (!node.right) node.right = newNode;
-          else insertNode(node.right, newNode);
-        }
-      }
-      insertNode(this.root, newNode);
+    else this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data <= node.data) {
+      if (!node.left) node.left = newNode;
+      else this.insertNode(node.left, newNode);
+    } else {
+      if (!node.right) node.right = newNode;
+      else this.insertNode(node.right, newNode);
     }
   }
 
   remove(data) {
-    this.root = this.removeNode(this.root, data); 
+    this.root = this.removeNode(this.root, data);
   }
 
   removeNode(node, data) {
-    if (!node) return null; 
+    if (!node) return null;
     else if (data < node.data) {
-      node.left = this.removeNode(node.left, data); 
-      return node; 
-    }
-    else if (data > node.data) {
-      node.right = this.removeNode(node.right, data); 
-      return node; 
-    }
-    else {
-      if (!node.left && !node.right) return null; 
-      else if (!node.left) return node.right; 
-      else if (!node.right) return node.left; 
+      node.left = this.removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.removeNode(node.right, data);
+      return node;
+    } else {
+      if (!node.left && !node.right) return null;
+      else if (!node.left) return node.right;
+      else if (!node.right) return node.left;
       else {
-        let minNode = this.getMin(node.right); 
-        node.data = minNode.data; 
+        let minNode = this.getMin(node.right);
+        node.data = minNode.data;
         node.right = this.removeNode(node.right, minNode.data);
         return node;
       }
@@ -67,26 +64,53 @@ class BinarySearchTree {
   preOrder(node) {
     if (node) {
       console.log(node.data);
-      this.inOrder(node.left);
-      this.inOrder(node.right);
+      this.preOrder(node.left);
+      this.preOrder(node.right);
     }
   }
+
   postOrder(node) {
     if (node) {
-      this.inOrder(node.left);
-      this.inOrder(node.right);
+      this.postOrder(node.left);
+      this.postOrder(node.right);
       console.log(node.data);
+    }
+  }
+
+  breadth(node) {
+    let queue = [];
+    queue.push(this.root);
+
+    while (queue.length > 0) {
+      let node = queue.shift();
+      console.log(node.data);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  depth(node) {
+    let stack = []; 
+    stack.push(this.root); 
+
+    while (stack.length > 0) {
+      let node = stack.pop(); 
+      console.log(node.data); 
+      if (node.right) stack.push(node.right); 
+      if (node.left) stack.push(node.left); 
     }
   }
 
   getMin(node) {
     if (!node) return null;
+
     if (!node.left) return node;
     else return this.getMin(node.left);
   }
 
   getMax(node) {
     if (!node) return null;
+
     if (!node.right) return node;
     else return this.getMax(node.right);
   }
@@ -94,19 +118,35 @@ class BinarySearchTree {
 
 let bst = new BinarySearchTree(); 
 
-// bst.insert(8); 
-// bst.insert(4); 
-// bst.insert(6);
-// bst.insert(5);
-// bst.insert(7);
-// bst.insert(2); 
-// bst.insert(12);
+bst.insert(5); 
+bst.insert(3); 
+bst.insert(2);
+bst.insert(4);
+bst.insert(7);
+bst.insert(6); 
+bst.insert(8);
+// bst.insert(9);
 // bst.insert(10);
 
 // bst.inOrder(bst.root); 
 // console.log(); 
 
+// bst.preOrder(bst.root);
+// console.log(); 
+
+// bst.depth(bst.root); 
+// console.log(); 
+
+// bst.postOrder(bst.root);
+// console.log(); 
+
+// bst.breadth(bst.root);
+// console.log(); 
+
 // bst.remove(6);
+
+console.log(bst.isBalanced(bst.root));
+
 // bst.inOrder(bst.root);
 // console.log(); 
 
