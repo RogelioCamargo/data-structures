8fd75a38012c192d6bcda569087b5201cea95d28
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-10-17 22:45:07 -0700

    Implement delete method for trie class

--- ds/Trie.js
+++ ds/Trie.js
@@ -1,4 +1,4 @@
-class TrieNode() {
+class TrieNode {
   constructor() {
     this.children = new Map(); 
     this.isWord = false; 
@@ -34,7 +34,7 @@ class Trie {
 
   search(word) {
     if (!word.length) return false; 
-    return this.searchNode(this.root, word, index); 
+    return this.searchNode(this.root, word, 0); 
   }
 
   searchNode(current, word, index) {
@@ -49,4 +49,49 @@ class Trie {
 
     return this.searchNode(charNode, word, index + 1); 
   }
+
+  delete(word) {
+    if (!word.length) return null; 
+    if (!this.search(word)) return null; 
+
+    this.deleteNode(this.root, word, 0); 
+  }
+
+  deleteNode(current, word, index) {
+    if (index === word.length) {
+      current.isWord = false;  
+      return null;
+    }
+
+    let char = word.charAt(index); 
+    let charNode = current.children.get(char); 
+
+    this.deleteNode(charNode, word, index + 1); 
+
+    if (!charNode.children.size && !charNode.isWord) {
+      current.children.delete(char); 
+    }
+  }
 }
+
+let trie = new Trie(); 
+
+trie.insert("car"); 
+// console.log("car: " + trie.search("car"));
+trie.insert("cart")
+// console.log("cart: " + trie.search("cart"));
+trie.insert("due");
+// console.log("due: " + trie.search("due"));
+console.log(trie.root.children); 
+
+trie.delete("duee"); 
+console.log("car: " + trie.search("car"));
+console.log("cart: " + trie.search("cart"));
+console.log("due: " + trie.search("due"));
+console.log(trie.root.children); 
+
+// console.log(trie.root.children); 
+// let nextNode = trie.root.children.get("c").children.get("a");
+// console.log(nextNode); 
+
+// console.log(nextNode.children.get("r"))
