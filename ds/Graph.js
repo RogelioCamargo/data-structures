9155a8a23b86f7ea5c7d439a3cf8e555b59ec48b
RogelioCamargo <rogelio.camargo2016@gmail.com>
2021-10-19 13:48:59 -0700

    Create GraphNode class

---
+++ ds/Graph.js
@@ -0,0 +1,26 @@
+class GraphNode {
+  constructor(value) {
+    this.value = value; 
+    this.adjacents = []; 
+  }
+
+  addAdjacent(node) { 
+    this.adjacents.push(node);
+  }
+
+  removeAdjacent(node) { 
+    let index = this.adjacents.indexOf(node); 
+    if (index > -1) {
+      this.adjacents.splice(index, 1); 
+      return node; 
+    }
+  }
+
+  getAdjacents() {
+    return this.adjacents; 
+  }
+
+  isAdjacent(node) {
+    return this.adjacents.indexOf(node) > -1; 
+  }
+}
